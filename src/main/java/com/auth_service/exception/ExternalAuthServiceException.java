84f56eae143440a8854e00ebb34e0832b472c0cf
Kuleb9lka <winkeylinks@gmail.com>
2026-03-03 15:35:38 +0300

    deleted ExternalAuthServiceException, RegistrationDetailsConflictException, UserMapper, UserRegisterDto

--- src/main/java/com/auth_service/exception/ExternalAuthServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.auth_service.exception;
-
-public class ExternalAuthServiceException extends RuntimeException {
-    public ExternalAuthServiceException(String message) {
-        super(message);
-    }
-}
